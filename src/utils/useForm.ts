01b5a5479df9f249058a496feaddbc57d502e72c
Piotr Kukla <piotr.kukla19@gmail.com>
2021-10-28 17:27:03 +0200

    Add useForm hook

---
+++ src/utils/useForm.ts
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const useForm = <T extends InitValues>(initValues: T) => {
+  const [values, setValues] = useState(() => initValues);
+  const handleChange: HandleChange = (e) => {
+    setValues((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const reset = () => setValues(initValues);
+
+  return { values, handleChange, reset };
+};
+
+interface InitValues {
+  [key: string]: string;
+}
+
+interface HandleChange {
+  (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void;
+}
+
+export default useForm;
