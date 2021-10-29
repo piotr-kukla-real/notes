a8e03505c91f8508988030390e146ee547a6dd8b
Piotr Kukla <piotr.kukla19@gmail.com>
2021-10-29 20:33:26 +0200

    Global styling Mui Components and move styles from theme to global CSS

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,15 +1,5 @@
 import { createTheme } from '@material-ui/core/styles';
 
-const shadow1 = '0px 1px 2px 1px hsla(0, 0%, 0%, 0.25)';
-const transitionOptions = '250ms cubic-bezier(0.4, 0, 0.2, 1)';
-const focusVisible = {
-  outline: '2px solid transparent',
-  transition: `outline-color ${transitionOptions}, background-color ${transitionOptions}, box-shadow ${transitionOptions}`,
-  '&.Mui-focusVisible': {
-    outlineColor: 'rgba(0, 0, 0, 0.5)',
-  },
-};
-
 const theme = createTheme({
   palette: {
     primary: {
@@ -54,49 +44,6 @@ const theme = createTheme({
     },
     MuiIconButton: {
       disableFocusRipple: true,
-    },
-  },
-  overrides: {
-    MuiOutlinedInput: {
-      root: {
-        '&:hover .MuiOutlinedInput-notchedOutline': {
-          borderColor: 'hsl(202, 100%, 50%)',
-        },
-      },
-    },
-    MuiButton: {
-      contained: {
-        boxShadow: shadow1,
-        outline: focusVisible.outline,
-        transition: focusVisible.transition,
-        '&:hover': {
-          boxShadow: shadow1,
-        },
-        '&:active': {
-          boxShadow: 'none',
-        },
-        '&.Mui-focusVisible': {
-          boxShadow: shadow1,
-          outlineColor: focusVisible['&.Mui-focusVisible'].outlineColor,
-        },
-      },
-    },
-    MuiFab: {
-      root: {
-        ...focusVisible,
-        '&:active': {
-          boxShadow: 'none',
-        },
-      },
-    },
-    MuiIconButton: {
-      root: {
-        ...focusVisible,
-        boxShadow: shadow1,
-        '&:active': {
-          boxShadow: 'none',
-        },
-      },
     },
   },
 });
