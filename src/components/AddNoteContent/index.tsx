7e06660afd78d923c37ef67720ebbc1d5e1477eb
Piotr Kukla <piotr.kukla19@gmail.com>
2021-10-29 16:44:07 +0200

    Add functionality to AddNoteForm

--- src/components/AddNoteContent/index.tsx
+++ src/components/AddNoteContent/index.tsx
@@ -1,10 +1,11 @@
 import AddNoteForm from 'components/AddNoteForm';
 import DialogTopBar from 'components/DialogTopBar';
+import FormPropProvider from 'components/FormPropProvider';
 
 const AddNoteContent: React.FC = () => (
   <section>
     <DialogTopBar header="Add note" />
-    <AddNoteForm />
+    <FormPropProvider form={AddNoteForm} />
   </section>
 );
 
--- src/components/AddNoteForm/index.tsx
+++ src/components/AddNoteForm/index.tsx
@@ -1,15 +1,87 @@
 import { Button, TextField } from '@material-ui/core';
 import { titleMaxLength, noteMaxLength } from 'utils/settings';
 import styles from 'utils/formStyles.module.scss';
+import useForm from 'utils/useForm';
+import type { FormProps } from 'components/FormPropProvider';
+import { useEffect, useRef } from 'react';
 
-const AddNoteForm: React.FC = () => {
+const initialValues = {
+  title: '',
+  note: '',
+};
+
+const handleInitValues = () => {
+  const check = localStorage.getItem('add-note');
+  if (check) {
+    const values: typeof initialValues = JSON.parse(check);
+    const { title, note } = values;
+
+    if (title.length > titleMaxLength) {
+      values.title = title.slice(0, titleMaxLength);
+    } else if (note.length > noteMaxLength) {
+      values.note = note.slice(0, noteMaxLength);
+    }
+
+    return values;
+  }
+  localStorage.setItem('add-note', JSON.stringify(initialValues));
+  return initialValues;
+};
+
+const AddNoteForm: React.FC<FormProps> = ({ setNotify, dispatch }) => {
+  const { values, handleChange, handleValues } = useForm(handleInitValues);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { title, note } = values;
+
+    if (!title || !note) {
+      setNotify({
+        message: 'You need to fill every field!',
+        color: 'error',
+        icon: 'error',
+      });
+      return;
+    }
+
+    dispatch({
+      type: 'ADD',
+      payload: { title, note },
+    });
+
+    setNotify({ message: 'Note added' });
+    handleValues(initialValues);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      (e.target as any).form[2].focus();
+    }
+  };
+
+  const noteHelperText = `${values.note.length}/${noteMaxLength}`;
+
+  const firstRender = useRef(true);
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+      return;
+    }
+    localStorage.setItem('add-note', JSON.stringify(values));
+  }, [values]);
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={onSubmit}>
       <TextField
         className={styles.formControl}
         label="title"
         name="title"
         inputProps={{ maxLength: titleMaxLength }}
+        value={values.title}
+        onChange={handleChange}
+        onKeyDown={onKeyDown}
       />
       <TextField
         className={styles.formControl}
@@ -18,6 +90,9 @@ const AddNoteForm: React.FC = () => {
         inputProps={{ maxLength: noteMaxLength }}
         multiline
         rows="8"
+        value={values.note}
+        onChange={handleChange}
+        helperText={noteHelperText}
       />
       <Button className={styles.submitBtn} type="submit">
         Add
